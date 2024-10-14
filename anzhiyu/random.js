bb9a00291239a4dcc64a43d9875b5547c3d01885
ChengCingSyuan <MikasaAkaman@protonmail.com>
2024-10-15 00:39:08 +0800

    Site updated: 2024-10-15 00:39:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/13/Netcup_Restallation/"];function toRandomPost(){
+var posts=["2024/10/14/nezha/","2024/10/13/Netcup_Restallation/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
