062ef4a053ca97be7e5efa4bc1b96adcc8a4312d
ChengCingSyuan <MikasaAkaman@protonmail.com>
2024-10-16 01:15:45 +0800

    Site updated: 2024-10-16 01:15:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/14/nezha/","2024/10/13/Netcup_Restallation/"];function toRandomPost(){
+var posts=["2024/10/14/nezha/","2024/10/13/Netcup_Restallation/","2024/10/15/PTbox-Installation/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
